6e180fb5e35238aee83008ca8c831d0aec1f98ea
Strongersakthi <132252842+Strongersakthi@users.noreply.github.com>
2023-05-01 12:52:40 +0530

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinch & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry oatmeal')
+streamlit.text('🐔Kale, Spinch & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
